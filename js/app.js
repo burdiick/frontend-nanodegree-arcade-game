e678497a717eb4f70164bdedee7ff1dc0e847b0c
Daniel Burdick <burdick.daniel@gmail.com>
2015-09-16 18:06:40 -0700

    add LevelObject parent object for all objects to be rendered on screen.

--- js/app.js
+++ js/app.js
@@ -2,21 +2,30 @@
 var CANVAS_WIDTH = 800;
 var CANVAS_HEIGHT = 600;
 
-var Enemy = function (enemy) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+var LevelObject = function(object, scaleX, scaleY, sprite) {
+  this.sprite = sprite;
+  this.x = object.x * scaleX;
+  console.log(object.x);
+  this.y = object.y * (scaleX * 0.79);
+  this.scaleX = scaleX;
+  this.scaleY = scaleY;
+  this.speed = object.speed;
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-  this.x = 101 * enemy.path[0].x;
-  this.y = 79 * enemy.path[0].y - 25;
-  this.speed = enemy.speed;
+}
+
+LevelObject.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.scaleX, this.scaleY);
+}
+
+var Enemy = function (enemy, scaleX, scaleY) {
+  LevelObject.call(this, enemy, scaleX, scaleY, 'images/enemy-bug.png');
+  console.log(this.x);
+  this.y = this.y - 25;
   this.path = enemy.path;
   this.currentWaypoint = enemy.path[0];
   console.log(enemy.type);
 };
-
+Enemy.prototype = Object.create(LevelObject.prototype);
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
@@ -26,37 +35,30 @@ Enemy.prototype.update = function (dt) {
 
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function (x, y) {
-  this.sprite = 'images/char-boy.png';
-  this.x = this.setX(x);
-  this.y = this.setY(y) - 15;
+var Player = function (player, scaleX, scaleY) {
+  LevelObject.call(this, player, scaleX, scaleY,
+  'images/char-boy.png');
+  this.y = this.y - 15;
   this.movement = {
     'move': ''
   };
 };
+Player.prototype = Object.create(LevelObject.prototype);
 
 Player.prototype.setX = function (multiplier) {
-  return multiplier * 101;
+  return multiplier * this.scaleX;
 };
 
 Player.prototype.getX = function () {
-  return this.x / 101;
+  return this.x / this.scaleX;
 };
 
 Player.prototype.setY = function (multiplier) {
-  return multiplier * 79;
+  return multiplier * (this.scaleX * 0.79);
 };
 
 Player.prototype.getY = function () {
-  return this.y / 79;
+  return this.y / (this.scaleX * 0.79);
 };
 
 Player.prototype.update = function () {
@@ -76,9 +78,9 @@ Player.prototype.update = function () {
   this.movement = '';
 };
 
-Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+//Player.prototype.render = function () {
+//  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//};
 
 Player.prototype.handleInput = function (key) {
   this.movement = key;
@@ -106,7 +108,9 @@ var levels = {
     ],
     'enemies': [{         // Data to instantiate enemies
       'type': 'red-bug',  // Enemy type
-      'speed': 0,         // Movement speed
+      'speed': 0,
+      'x': 0,
+      'y': 1,         // Movement speed
       'path': [{          // Array of grid locations to use as waypoints
         'x': 0,
         'y': 1
@@ -117,6 +121,8 @@ var levels = {
     }, {
       'type': 'red-bug',
       'speed': 2,
+      'x': 4,
+      'y': 3,
       'path': [{
         'x': 4,
         'y': 3
@@ -147,6 +153,8 @@ var levels = {
     'enemies': [{         // Data to instantiate enemies
       'type': 'red-bug',  // Enemy type
       'speed': 0,         // Movement speed
+      'x': 4,
+      'y': 3,
       'path': [{          // Array of grid locations to use as waypoints
         'x': 0,
         'y': 1
@@ -157,6 +165,8 @@ var levels = {
     }, {
       'type': 'red-bug',
       'speed': 2,
+      'x': 10,
+      'y': 6,
       'path': [{
         'x': 4,
         'y': 3
@@ -179,17 +189,8 @@ var Level = function(number) {
   this.levelNumber = number;
   this.mapSize = level.mapSize;
   this.map = level.map;
-  this.enemies = level.enemies.map(function (enemy) {
-    console.log(enemy);
-    return new Enemy(enemy);
-  });
-  this.player = new Player(level.player.x, level.player.y);
-  this.helpText = level.helpText;
-  this.setScale();
-  console.log(this.scaleX);
-}
-
-Level.prototype.setScale = function() {
+  this.scaleX = 0;
+  this.scaleY = 0;
   if ((this.mapSize.cols / this.mapSize.rows) < CANVAS_WIDTH / CANVAS_HEIGHT) {
     this.scaleX = CANVAS_HEIGHT / this.mapSize.rows;
     this.scaleY = this.scaleX * 1.59;
@@ -198,6 +199,19 @@ Level.prototype.setScale = function() {
     console.log(this.mapSize.cols, this.scaleX);
     this.scaleY = this.scaleX * 1.59;
   }
+
+  this.enemies = level.enemies.map(function (enemy, scaleX, scaleY) {
+    console.log(this.scaleX + "In level create");
+    return new Enemy(enemy, 101, 171);
+  });
+  this.player = new Player(level.player, 101, 171);
+  this.helpText = level.helpText;
+
+  console.log(this.scaleX);
+}
+
+Level.prototype.setScale = function() {
+
 }
 
 Level.prototype.render = function() {
@@ -222,7 +236,7 @@ Level.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var level = new Level(1);
+var level = new Level(2);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
